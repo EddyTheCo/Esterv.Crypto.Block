67bb90cd5ac6e63fa2410a19dd31445eaba6aebc
EddyGlezz <eddyglezz95@gmail.com>
2023-01-01 19:02:00 +0100

    added function for returning especific feature and unlock and also the function fo calculate the min deposit amount

--- include/block/qoutputs.hpp
+++ include/block/qoutputs.hpp
@@ -31,9 +31,22 @@ namespace qiota{
 				Basic_Output(const QJsonValue& val);
 				Basic_Output(QDataStream &in);
 				void serialize(QDataStream &out)const;
-
+                quint64 min_deposit_of_output(const quint64 &wkey, const quint64 &wdata, const quint64 &v_byte_cost)const;
 				QJsonObject get_Json(void) const;
 				quint64 amount(void)const{return amount_;};
+                void set_amount(quint64 amount_m){amount_=amount_m;}
+                std::shared_ptr<Unlock_Condition> get_unlock_(const quint8 typ)const
+                {
+                    const auto found=std::find_if(unlock_conditions_.begin(),unlock_conditions_.end(),
+                                                  [typ](const auto& it){return (it->type_m==typ);});
+                    return (found==unlock_conditions_.end())?nullptr:*found;
+                }
+                std::shared_ptr<Feature> get_feature_(const quint8 typ)const
+                {
+                    const auto found=std::find_if(features_.begin(),features_.end(),
+                                                  [typ](const auto& it){return (it->type_m==typ);});
+                    return (found==features_.end())?nullptr:*found;
+                }
 				std::vector<std::shared_ptr<Feature>> features(void)const{return features_;};
 				std::vector<std::shared_ptr<Unlock_Condition>> unlock_conditions(void)const{return unlock_conditions_;};
 					
