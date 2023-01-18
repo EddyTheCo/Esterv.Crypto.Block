3bbad8c00b62f6f7f8ab15e90cd6f483ecd15555
EddyGlezz <eddyglezz95@gmail.com>
2023-01-18 19:39:47 +0100

    added NFT output

--- include/block/qoutputs.hpp
+++ include/block/qoutputs.hpp
@@ -10,57 +10,93 @@
 #include"block/qfeatures.hpp"
 #include"block/qnative_tokens.hpp"
 namespace qiota{
-	namespace qblocks{
+namespace qblocks{
 
-		class Output
-		{
-			public:
-            enum types : quint8 { Basic_typ=3 };
-                Output(types typ );
-				template<class from_type> static std::shared_ptr<Output> from_(from_type& val);
-				virtual void serialize(QDataStream &out)const;
-				virtual QJsonObject get_Json(void) const;
-                const types type_m;
-		};
+class Output
+{
+public:
+    enum types : quint8 { Basic_typ=3,NFT_typ=6 };
+    Output(types typ,quint64 amount_m, const std::vector<std::shared_ptr<Unlock_Condition>> & unlock_conditions_m,
+           const std::vector<std::shared_ptr<Feature>>& features_m,
+           const std::vector<std::shared_ptr<Native_Token>>& native_tokens_m );
 
-		class Basic_Output :public Output
-		{
-			public:
-				Basic_Output(quint64 amount_m, const std::vector<std::shared_ptr<Unlock_Condition>> & unlock_conditions_m,
-						const std::vector<std::shared_ptr<Feature>>& features_m,
-						const std::vector<std::shared_ptr<Native_Token>>& native_tokens_m);
-				Basic_Output(const QJsonValue& val);
-				Basic_Output(QDataStream &in);
-				void serialize(QDataStream &out)const;
-                quint64 min_deposit_of_output(const quint64 &wkey, const quint64 &wdata, const quint64 &v_byte_cost)const;
-				QJsonObject get_Json(void) const;
-				quint64 amount(void)const{return amount_;};
-                void set_amount(quint64 amount_m){amount_=amount_m;}
-                std::shared_ptr<Unlock_Condition> get_unlock_(const Unlock_Condition::types& typ)const
-                {
-                    const auto found=std::find_if(unlock_conditions_.begin(),unlock_conditions_.end(),
-                                                  [typ](const auto& it){return (it->type_m==typ);});
-                    return (found==unlock_conditions_.end())?nullptr:*found;
-                }
+    Output(types typ,const QJsonValue& val);
+    Output(types typ):type_m(typ){};
 
-                std::shared_ptr<Feature> get_feature_(const Feature::types& typ)const
-                {
-                    const auto found=std::find_if(features_.begin(),features_.end(),
-                                                  [typ](const auto& it){return (it->type_m==typ);});
-                    return (found==features_.end())?nullptr:*found;
-                }
-				std::vector<std::shared_ptr<Feature>> features(void)const{return features_;};
-				std::vector<std::shared_ptr<Unlock_Condition>> unlock_conditions(void)const{return unlock_conditions_;};
-					
+    template<class from_type> static std::shared_ptr<Output> from_(from_type& val);
+    virtual void serialize(QDataStream &out)const;
+    virtual QJsonObject get_Json(void) const;
 
-			private:
+    quint64 min_deposit_of_output(const quint64 &wkey, const quint64 &wdata, const quint64 &v_byte_cost)const;
 
-				std::vector<std::shared_ptr<Unlock_Condition>> unlock_conditions_;
-				std::vector<std::shared_ptr<Feature>> features_;
-				std::vector<std::shared_ptr<Native_Token>> native_tokens_;
-				quint64 amount_;
-		};
+    void serialize_native_tokens(QDataStream &out)const
+    {
+        serialize_list<quint8>(out,native_tokens_);
+    }
+    void serialize_unlock_conditions(QDataStream &out)const
+    {
+        serialize_list<quint8>(out,unlock_conditions_);
+    }
+    void serialize_features_(QDataStream &out)const
+    {
+        serialize_list<quint8>(out,features_);
+    }
 
-	};
+    std::shared_ptr<Unlock_Condition> get_unlock_(const Unlock_Condition::types& typ)const
+    {
+        const auto found=std::find_if(unlock_conditions_.begin(),unlock_conditions_.end(),
+                                      [typ](const auto& it){return (it->type_m==typ);});
+        return (found==unlock_conditions_.end())?nullptr:*found;
+    }
+
+    std::shared_ptr<Feature> get_feature_(const Feature::types& typ)const
+    {
+        const auto found=std::find_if(features_.begin(),features_.end(),
+                                      [typ](const auto& it){return (it->type_m==typ);});
+        return (found==features_.end())?nullptr:*found;
+    }
+
+
+    const types type_m;
+    quint64 amount_;
+    std::vector<std::shared_ptr<Unlock_Condition>> unlock_conditions_;
+    std::vector<std::shared_ptr<Feature>> features_;
+    std::vector<std::shared_ptr<Native_Token>> native_tokens_;
+
+};
+
+class Basic_Output :public Output
+{
+public:
+    Basic_Output(quint64 amount_m, const std::vector<std::shared_ptr<Unlock_Condition>> & unlock_conditions_m,
+                 const std::vector<std::shared_ptr<Feature>>& features_m,
+                 const std::vector<std::shared_ptr<Native_Token>>& native_tokens_m);
+    Basic_Output(const QJsonValue& val);
+    Basic_Output(QDataStream &in);
+    void serialize(QDataStream &out)const;
+    QJsonObject get_Json(void) const;
+
+
+};
+class NFT_Output :public Output
+{
+public:
+    NFT_Output(quint64 amount_m, const std::vector<std::shared_ptr<Unlock_Condition>> & unlock_conditions_m,
+               const std::vector<std::shared_ptr<Feature>>& features_m,
+               const std::vector<std::shared_ptr<Native_Token>>& native_tokens_m,
+               std::vector<std::shared_ptr<Feature>> immutable_features_m=std::vector<std::shared_ptr<Feature>>{});
+    NFT_Output(const QJsonValue& val);
+    NFT_Output(QDataStream &in);
+    void serialize(QDataStream &out)const;
+    QJsonObject get_Json(void) const;
+    std::vector<std::shared_ptr<Feature>> immutable_features_;
+    auto nft_id(void)const{return nft_id_;}
+private:
+    NFT_ID nft_id_;
+
+
+};
+
+};
 
 };
--- src/qoutputs.cpp
+++ src/qoutputs.cpp
@@ -3,88 +3,16 @@
 namespace qiota{
 namespace qblocks{
 void Output::serialize(QDataStream &out)const{};
-QJsonObject Output::get_Json(void) const{return QJsonObject();};
-Output::Output(types typ):type_m(typ){};
-template<class from_type>  std::shared_ptr<Output> Output::from_(from_type& val){
-    const auto type_=get_type<types>(val);
-    switch(type_) {
-      case Basic_typ:
-        return std::shared_ptr<Output>(new Basic_Output(val));
-    default:
-    return nullptr;
-
-    }
-}
-template std::shared_ptr<Output> Output::from_<const QJsonValue>(const QJsonValue& val);
-template std::shared_ptr<Output> Output::from_<QDataStream >(QDataStream & val);
-template std::shared_ptr<Output> Output::from_<const QJsonValueRef>(const QJsonValueRef& val);
-
-
-
-
-Basic_Output::Basic_Output(quint64 amount_m, const std::vector<std::shared_ptr<Unlock_Condition>> & unlock_conditions_m,
-             const std::vector<std::shared_ptr<Feature >> & features_m,
-             const std::vector<std::shared_ptr<Native_Token>> & native_tokens_m):Output(types::Basic_typ),amount_(amount_m),
-    unlock_conditions_(unlock_conditions_m),features_(features_m),native_tokens_(native_tokens_m){};
-
-Basic_Output::Basic_Output(const QJsonValue& val):Basic_Output(
-                               val.toObject()["amount"].toString().toULongLong(),
-    get_T<Unlock_Condition>(val.toObject()["unlockConditions"].toArray()),
-    get_T<Feature>(val.toObject()["features"].toArray()),
-    get_T<Native_Token>(val.toObject()["nativeTokens"].toArray())
-                                                      ){
-};
-Basic_Output::Basic_Output(QDataStream &in):Output(types::Basic_typ)
-{
-    in>>amount_;
-    quint8  native_tokens_count;
-    in>>native_tokens_count;
-    for(auto i=0;i<native_tokens_count;i++)
-    {
-        native_tokens_.push_back(Native_Token::from_<QDataStream>(in));
-    }
-    quint8 unlock_conditions_count;
-    in>>unlock_conditions_count;
-    for(auto i=0;i<unlock_conditions_count;i++)
-    {
-        unlock_conditions_.push_back(Unlock_Condition::from_<QDataStream>(in));
-    }
-    quint8 features_count;
-    in>>features_count;
-    for(auto i=0;i<features_count;i++)
-    {
-        features_.push_back(Feature::from_<QDataStream>(in));
-    }
-}
-void Basic_Output::serialize(QDataStream &out)const
-{
-    out<<type_m<<amount_;
-    out<<static_cast<quint8>(native_tokens_.size());
-    for(const auto& v: native_tokens_)v->serialize(out);
-
-    out<<static_cast<quint8>(unlock_conditions_.size());
-    for(const auto& v: unlock_conditions_)v->serialize(out);
-    out<<static_cast<quint8>(features_.size());
-    for(const auto& v: features_)v->serialize(out);
-}
-quint64 Basic_Output::min_deposit_of_output(const quint64& wkey,const quint64& wdata,const quint64& v_byte_cost)const
-{
-    quint64 offset=34*wkey+(32+4+4)*wdata;
-    c_array serializedoutput;
-    serializedoutput.from_object<Output>(*this);
-    quint64 outp=serializedoutput.size()*wdata;
-    return (outp+offset)*v_byte_cost;
-}
-QJsonObject Basic_Output::get_Json(void) const
+QJsonObject Output::get_Json(void) const
 {
     QJsonObject var;
     var.insert("type",(int)type_m);
-    var.insert("amount",QString::number(amount_)); //fix this
+    var.insert("amount",QString::number(amount_));
     if(native_tokens_.size())
     {
-         QJsonArray naarr;
-         for(const auto& v: native_tokens_)naarr.push_back(v->get_Json());
-         var.insert("nativeTokens",naarr);
+        QJsonArray naarr;
+        for(const auto& v: native_tokens_)naarr.push_back(v->get_Json());
+        var.insert("nativeTokens",naarr);
     }
 
     QJsonArray unarr;
@@ -92,10 +20,116 @@ QJsonObject Basic_Output::get_Json(void) const
     var.insert("unlockConditions",unarr);
     if(features_.size())
     {
-         QJsonArray fearr;
-         for(const auto& v: features_)fearr.push_back(v->get_Json());
-         var.insert("features",fearr);
+        QJsonArray fearr;
+        for(const auto& v: features_)fearr.push_back(v->get_Json());
+        var.insert("features",fearr);
     }
+    return var;
+};
+Output::Output(types typ, quint64 amount_m,
+               const std::vector<std::shared_ptr<Unlock_Condition> > &unlock_conditions_m,
+               const std::vector<std::shared_ptr<Feature> > &features_m,
+               const std::vector<std::shared_ptr<Native_Token> > &native_tokens_m):type_m(typ),
+    amount_(amount_m), unlock_conditions_(unlock_conditions_m),features_(features_m),native_tokens_(native_tokens_m){};
+template<class from_type>  std::shared_ptr<Output> Output::from_(from_type& val){
+    const auto type_=get_type<types>(val);
+    switch(type_) {
+    case Basic_typ:
+        return std::shared_ptr<Output>(new Basic_Output(val));
+    default:
+        return nullptr;
+
+    }
+}
+template std::shared_ptr<Output> Output::from_<const QJsonValue>(const QJsonValue& val);
+template std::shared_ptr<Output> Output::from_<QDataStream >(QDataStream & val);
+template std::shared_ptr<Output> Output::from_<const QJsonValueRef>(const QJsonValueRef& val);
+
+quint64 Output::min_deposit_of_output(const quint64& wkey,const quint64& wdata,const quint64& v_byte_cost)const
+{
+    quint64 offset=34*wkey+(32+4+4)*wdata;
+    c_array serializedoutput;
+    serializedoutput.from_object<Output>(*this);
+    quint64 outp=serializedoutput.size()*wdata;
+    return (outp+offset)*v_byte_cost;
+}
+
+Output::Output(types typ,const QJsonValue& val):Output(typ,val.toObject()["amount"].toString().toULongLong(),
+    get_T<Unlock_Condition>(val.toObject()["unlockConditions"].toArray()),
+    get_T<Feature>(val.toObject()["features"].toArray()),
+    get_T<Native_Token>(val.toObject()["nativeTokens"].toArray())
+  ){
+};
+
+Basic_Output::Basic_Output(quint64 amount_m, const std::vector<std::shared_ptr<Unlock_Condition>> & unlock_conditions_m,
+                           const std::vector<std::shared_ptr<Feature >> & features_m,
+                           const std::vector<std::shared_ptr<Native_Token>> & native_tokens_m):Output(types::Basic_typ,amount_m,
+                                                                                                      unlock_conditions_m,
+                                                                                                      features_m,
+                                                                                                      native_tokens_m
+                                                                                                      ){};
+
+Basic_Output::Basic_Output(const QJsonValue& val):Output(types::Basic_typ,val)
+{
+};
+
+Basic_Output::Basic_Output(QDataStream &in):Output(types::Basic_typ)
+{
+    in>>amount_;
+    native_tokens_=deserialize_list<quint8,Native_Token>(in);
+    unlock_conditions_=deserialize_list<quint8,Unlock_Condition>(in);
+    features_=deserialize_list<quint8,Feature>(in);
+}
+void Basic_Output::serialize(QDataStream &out)const
+{
+    out<<type_m<<amount_;
+    serialize_native_tokens(out);
+    serialize_unlock_conditions(out);
+    serialize_features_(out);
+}
+
+QJsonObject Basic_Output::get_Json(void) const
+{
+    return this->Output::get_Json();
+}
+
+
+
+NFT_Output::NFT_Output(quint64 amount_m, const std::vector<std::shared_ptr<Unlock_Condition>> & unlock_conditions_m,
+                       const std::vector<std::shared_ptr<Feature >> & features_m,
+                       const std::vector<std::shared_ptr<Native_Token>> & native_tokens_m,
+                       std::vector<std::shared_ptr<Feature>> immutable_features_m):Output(types::NFT_typ,amount_m,
+                                                                                          unlock_conditions_m,
+                                                                                          features_m,
+                                                                                          native_tokens_m),immutable_features_(immutable_features_m){};
+
+
+NFT_Output::NFT_Output(const QJsonValue& val):Output(types::NFT_typ,val),
+    immutable_features_(get_T<Feature>(val.toObject()["immutableFeatures"].toArray())),
+    nft_id_(NFT_ID(val.toObject()["nftId"]))
+{}
+
+NFT_Output::NFT_Output(QDataStream &in):Output(types::NFT_typ)
+{
+    in>>amount_;
+    native_tokens_=deserialize_list<quint8,Native_Token>(in);
+    nft_id_=NFT_ID(32,0);
+    in>>nft_id_;
+    unlock_conditions_=deserialize_list<quint8,Unlock_Condition>(in);
+    features_=deserialize_list<quint8,Feature>(in);
+    immutable_features_=deserialize_list<quint8,Feature>(in);
+}
+
+QJsonObject NFT_Output::get_Json(void) const
+{
+    auto var=this->Output::get_Json();
+    if(immutable_features_.size())
+    {
+        QJsonArray fearr;
+        for(const auto& v: immutable_features_)fearr.push_back(v->get_Json());
+        var.insert("immutableFeatures",fearr);
+    }
+    var.insert("nftId",nft_id_.toHexString());
     return var;
 }
 
