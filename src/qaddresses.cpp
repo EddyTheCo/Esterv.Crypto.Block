7b17703016497c001bb62137905d4f61150b2691
EddyGlezz <eddyglezz95@gmail.com>
2023-04-04 21:26:29 +0200

    use base funtion if posible

--- src/qaddresses.cpp
+++ src/qaddresses.cpp
@@ -1,9 +1,18 @@
 #include"block/qaddresses.hpp"
 namespace qiota{
 namespace qblocks{
-void Address::serialize(QDataStream &out)const{};
+void Address::serialize(QDataStream &out)const
+{
+    out<<type_m;
+    out<<addrhash_;
+};
 QJsonObject Address::get_Json(void) const{return QJsonObject();};
-Address::Address(types typ ):type_m(typ){};
+Address::Address(types typ, c_array addrhash_m ):type_m(typ),addrhash_(addrhash_m){};
+Address::Address(types typ, QDataStream &in):type_m(typ)
+{
+    addrhash_=c_array(32,0);
+    in>>addrhash_;
+}
 template<class from_type> std::shared_ptr<Address> Address::from_(from_type& val){
     const auto type_=get_type<types>(val);
     switch(type_) {
@@ -30,64 +39,40 @@ std::shared_ptr<Address> Address::from_(QByteArray& val)
     return qblocks::Address::from_<QDataStream>(buffer);
 }
 
-NFT_Address::NFT_Address(c_array nft_id_m):Address(NFT_typ),nft_id_(nft_id_m){};
+NFT_Address::NFT_Address(c_array nft_id_m):Address(NFT_typ,nft_id_m){};
 NFT_Address::NFT_Address(const QJsonValue& val):NFT_Address(NFT_ID(val.toObject()["nftId"])){};
-NFT_Address::NFT_Address(QDataStream &in):Address(NFT_typ)
-{
-    nft_id_=c_array(32,0);
-    in>>nft_id_;
-}
-void NFT_Address::serialize(QDataStream &out)const
-{
-    out<<type_m;
-    out<<nft_id_;
-}
+NFT_Address::NFT_Address(QDataStream &in):Address(NFT_typ,in){};
+
 QJsonObject NFT_Address::get_Json(void) const
 {
     QJsonObject var;
-    var.insert("type",(int)type_m);
-    var.insert("nftId",nft_id_.toHexString());
+    var.insert("type",(int)type());
+    var.insert("nftId",addrhash().toHexString());
     return var;
 }
 
-Alias_Address::Alias_Address(c_array alias_id_m):Address(Alias_typ),alias_id_(alias_id_m){};
+Alias_Address::Alias_Address(c_array alias_id_m):Address(Alias_typ,alias_id_m){};
 Alias_Address::Alias_Address(const QJsonValue& val):Alias_Address(c_array(val.toObject()["aliasId"])){};
-Alias_Address::Alias_Address(QDataStream &in):Address(Alias_typ)
-{
-    alias_id_=c_array(32,0);
-    in>>alias_id_;
-}
+Alias_Address::Alias_Address(QDataStream &in):Address(Alias_typ,in){};
 
-void Alias_Address::serialize(QDataStream &out)const
-{
-    out<<type_m;
-    out<<alias_id_;
-}
+
 QJsonObject Alias_Address::get_Json(void) const
 {
     QJsonObject var;
-    var.insert("type",(int)type_m);
-    var.insert("aliasId",alias_id_.toHexString());
+    var.insert("type",(int)type());
+    var.insert("aliasId",addrhash().toHexString());
     return var;
 }
 
-Ed25519_Address::Ed25519_Address(c_array pubkeyhash_m):Address(Ed25519_typ),pubkeyhash_(pubkeyhash_m){};
+Ed25519_Address::Ed25519_Address(c_array pubkeyhash_m):Address(Ed25519_typ,pubkeyhash_m){};
 Ed25519_Address::Ed25519_Address(const QJsonValue& val):Ed25519_Address(c_array(val.toObject()["pubKeyHash"])){};
-Ed25519_Address::Ed25519_Address(QDataStream &in):Address(Ed25519_typ)
-{
-    pubkeyhash_=c_array(32,0);
-    in>>pubkeyhash_;
-}
-void Ed25519_Address::serialize(QDataStream &out)const
-{
-    out<<type_m;
-    out<<pubkeyhash_;
-}
+Ed25519_Address::Ed25519_Address(QDataStream &in):Address(Ed25519_typ,in){};
+
 QJsonObject Ed25519_Address::get_Json(void) const
 {
     QJsonObject var;
-    var.insert("type",(int)type_m);
-    var.insert("pubKeyHash",pubkeyhash_.toHexString());
+    var.insert("type",(int)type());
+    var.insert("pubKeyHash",addrhash().toHexString());
     return var;
 }
 };
