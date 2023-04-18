b3685686ae6a4680f2b51bfd5c8e052b8cfdcc28
EddyGlezz <eddyglezz95@gmail.com>
2023-04-18 17:30:01 +0200

    added a json return value

--- include/block/qoutput_metadata_response.hpp
+++ include/block/qoutput_metadata_response.hpp
@@ -11,6 +11,15 @@ class  Output_Metadata_Response
 
 public:
     Output_Metadata_Response(const QJsonValue& val);
+    QJsonObject get_Json()const{
+        QJsonObject var;
+        var.insert("blockId",block_id_.toHexString());
+        var.insert("transactionId",transaction_id_.toHexString());
+        var.insert("outputIndex",output_index_);
+        var.insert("isSpent",is_spent_);
+        var.insert("outputId",outputid_.toHexString());
+        return var;
+    }
     const Block_ID block_id_;
     const Transaction_ID transaction_id_;
     const quint16 output_index_;
@@ -22,6 +31,7 @@ public:
 
 private:
     Output_ID set_outputid(void) const;
+
 };
 
 
