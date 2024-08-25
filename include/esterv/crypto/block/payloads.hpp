bf8cd3899ed9d98520af369563fc3f7ca612e6dc
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-08-25 10:38:27 +0200

    add Signed transaction payload

--- include/esterv/crypto/block/payloads.hpp
+++ include/esterv/crypto/block/payloads.hpp
@@ -88,19 +88,108 @@ class SignedTransactionPayload : public Payload
     pvector<const Input> m_inputs;
     pvector<const Output> m_outputs;
     pvector<const ContextInput> m_conxtextInputs;
-    std::shared_ptr<const TaggedDataPayload> m_payload;
+    std::shared_ptr<const Payload> m_payload;
     pvector<const Unlock> m_unlocks;
+    fl_array<quint8> m_capabilities;
 
-    SignedTransactionPayload(const std::shared_ptr<const Essence> &essence_m, const pvector<const Unlock> &unlocks_m);
-    SignedTransactionPayload(const QJsonValue &val);
-    SignedTransactionPayload(QDataStream &in);
+    SignedTransactionPayload(const quint64 &networkId, const quint32 &creationSlot, const pvector<const Input> &inputs,
+                             const pvector<const Output> &outputs, const pvector<const Unlock> &unlocks,
+                             const pvector<const ContextInput> &conxtextInputs = {},
+                             const std::map<ID, quint64> &allotments = {},
+                             const fl_array<quint8> capabilities = fl_array<quint8>(),
+                             const std::shared_ptr<const Payload> &payload = nullptr)
+        : Payload{PayloadType::SignedTransaction}, m_networkId{networkId}, m_creationSlot{creationSlot},
+          m_inputs{inputs}, m_outputs{outputs}, m_unlocks{unlocks}, m_conxtextInputs{conxtextInputs},
+          m_allotments{allotments}, m_capabilities{capabilities}, m_payload{payload}
+    {
+    }
+    SignedTransactionPayload(const QJsonValue &val)
+        : Payload{PayloadType::SignedTransaction},
+          m_networkId{
+              static_cast<quint64>((val.toObject()["transaction"].toObject())["networkId"].toString().toLongLong())},
+          m_creationSlot{static_cast<quint32>((val.toObject()["transaction"].toObject())["creationSlot"].toInteger())},
+          m_inputs{getTvec<Input>((val.toObject()["transaction"].toObject())["inputs"].toArray())},
+          m_outputs{getTvec<Output>((val.toObject()["transaction"].toObject())["outputs"].toArray())},
+          m_unlocks{getTvec<Unlock>(val.toObject()["unlocks"].toArray())},
+          m_conxtextInputs{
+              getTvec<ContextInput>((val.toObject()["transaction"].toObject())["contextInputs"].toArray())},
+          m_capabilities{(val.toObject()["transaction"].toObject())["capabilities"]},
+          m_payload{Payload::from<const QJsonValue>((val.toObject()["transaction"].toObject())["payload"])}
+    {
+        const auto allotments = (val.toObject()["transaction"].toObject())["allotments"].toArray();
+        for (const auto &v : allotments)
+        {
+            const auto vObject = v.toObject();
+            m_allotments.insert({c_array{vObject["accountId"].toString()}, vObject["mana"].toString().toULongLong()});
+        }
+    }
+    SignedTransactionPayload(QDataStream &in) : Payload{PayloadType::SignedTransaction}
+    {
+        in >> m_networkId;
+        in >> m_creationSlot;
+        m_conxtextInputs = deserializeVector<quint16, ContextInput>(in);
+        m_inputs = deserializeVector<quint16, Input>(in);
+
+        quint16 length;
+        auto id = ID(ByteSizes::hash, 0);
+        quint64 mana;
+        in >> length;
+        for (auto i = 0; i < length; i++)
+        {
+            in >> id;
+            in >> mana;
+            m_allotments.insert({id, mana});
+        }
+        in >> m_capabilities;
+        quint32 payloadLength;
+        in >> payloadLength;
+
+        if (payloadLength != 0u)
+        {
+            m_payload = Payload::from<QDataStream>(in);
+        }
+        m_outputs = deserializeVector<quint16, Output>(in);
+        m_unlocks = deserializeVector<quint16, Unlock>(in);
+    }
 
   public:
     void serialize(QDataStream &out) const override
     {
+        Payload::serialize(out);
+        out << m_networkId;
+        out << m_creationSlot;
+        serializeList<quint16>(out, m_conxtextInputs);
+        serializeList<quint16>(out, m_inputs);
+        quint16 lenght = m_allotments.size();
+        out << lenght;
+        for (const auto &v : m_allotments)
+        {
+            out << v.first << v.second;
+        }
+        out << m_capabilities;
+        if (m_payload)
+        {
+            c_array serialized_payload;
+            serialized_payload.fromObject(m_payload);
+            out << static_cast<quint32>(serialized_payload.size());
+            out << serialized_payload;
+        }
+        serializeList<quint16>(out, m_outputs);
+        serializeList<quint16>(out, m_unlocks);
     }
     void addJson(QJsonObject &var) const override
     {
+        Payload::addJson(var);
+
+        QJsonObject transaction;
+        transaction.insert("networkId", QString::number(m_networkId));
+        transaction.insert("creationSlot", (int)m_creationSlot);
+        transaction.insert("contextInputs", jsonList(m_conxtextInputs));
+        transaction.insert("inputs", jsonList(m_inputs));
+        transaction.insert("allotments", jsonList(m_allotments));
+        var.insert("transaction", transaction);
+        var.insert("unlocks", jsonList(m_unlocks));
     }
+    friend class Payload;
 };
 }; // namespace esterv::crypto::block
--- src/payloads.cpp
+++ src/payloads.cpp
@@ -1,5 +1,4 @@
 #include <QCryptographicHash>
-#include <esterv/crypto/block/essences.hpp>
 #include <esterv/crypto/block/payloads.hpp>
 
 namespace esterv::crypto::block
@@ -13,16 +12,16 @@ template <class from_type> std::shared_ptr<const Payload> Payload::from(from_typ
     {
     case PayloadType::SignedTransaction:
         return std::shared_ptr<const Payload>(new SignedTransactionPayload(val));
-    case Tagged_Data_typ:
-        return std::shared_ptr<Payload>(new Tagged_Data_Payload(val));
+    case PayloadType::TaggedData:
+        return std::shared_ptr<const Payload>(new TaggedDataPayload(val));
     default:
         return nullptr;
     }
 }
 
-template std::shared_ptr<const Payload> Payload::from_<const QJsonValue>(const QJsonValue &val);
-template std::shared_ptr<const Payload> Payload::from_<QDataStream>(QDataStream &val);
-template std::shared_ptr<const Payload> Payload::from_<const QJsonValueRef>(const QJsonValueRef &val);
+template std::shared_ptr<const Payload> Payload::from<const QJsonValue>(const QJsonValue &val);
+template std::shared_ptr<const Payload> Payload::from<QDataStream>(QDataStream &val);
+template std::shared_ptr<const Payload> Payload::from<const QJsonValueRef>(const QJsonValueRef &val);
 
 std::shared_ptr<const Payload> Payload::TaggedData(const tagF &tag, const dataF &data)
 {
@@ -52,32 +51,5 @@ Transaction_Payload::Transaction_Payload(QDataStream &in)
         unlocks_.push_back(Unlock::from_<QDataStream>(in));
     }
 }
-void Transaction_Payload::serialize(QDataStream &out) const
-{
-    out << type();
-    essence_->serialize(out);
-    out << static_cast<quint16>(unlocks_.size());
-    for (const auto &v : unlocks_)
-        v->serialize(out);
-}
-QJsonObject Transaction_Payload::get_Json(void) const
-{
-    QJsonObject var;
-    var.insert("type", (int)type());
-    var.insert("essence", essence_->get_Json());
-    QJsonArray unloArray;
-    for (const auto &v : unlocks_)
-        unloArray.push_back(v->get_Json());
 
-    var.insert("unlocks", unloArray);
-
-    return var;
-}
-c_array Transaction_Payload::get_id(void) const
-{
-    c_array var;
-    var.from_object(*this);
-    return QCryptographicHash::hash(var, QCryptographicHash::Blake2b_256);
-}
 }; // namespace qblocks
-}; // namespace qiota
